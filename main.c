9ccfcdd878a4137b628967edc93c8ca2e53c757e
natani3l <nataniel.lemes@estudante.uffs.edu.br>
2021-03-19 21:46:00 -0300

    add: get all columns from csv and insert into bd

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <libpq-fe.h>
 #include <string.h>
 #include <time.h>
-#include <sys/stat.h>
 
 #define buffer_size 1048
 // gcc -o lib_version main.c  -I/usr/include/postgresql -lpq
@@ -28,42 +27,59 @@ void explicitTransition(PGconn *conn, PGresult *res, FILE* fp) {
 			PQclear(res);
 			do_exit(conn);
 		}
-
+		int i = 0;
 		begin = clock();
-
-		while (fgets(buffer, buffer_size, fp)) { //talvez getline()
-			char str[buffer_size] = "INSERT INTO product (product_name) VALUES ('";
+		while (fgets(buffer, buffer_size, fp)) {
+			char str[buffer_size] = "INSERT INTO product (";
+			char str2[buffer_size] = ") VALUES (";
 			column = 0; 
 			row++; 
 			if (row == 1) 
 				continue; 
 
-			char* value = strtok(buffer, "\t"); 
+			char* value = strtok(buffer, ",");
+			while (value)  {
+				if (column == 0 && strcmp (";", value) != 0) {
+					strcat(str, "product_name");
+					strcat(str2, "'");
+					strcat(str2, value);
+					strcat(str2, "'");
+				
+				} 
 
-			while (value) {
-				strcat(str, value);
-				strcat(str, "')");
+				if (column == 1 && strcmp (";", value) != 0) {
+					strcat(str, ", brand_name");
+					strcat(str2, ", '");
+					strcat(str2, value);
+					strcat(str2, "'");
+				} 
 
-				res = PQexec(conn, str);
+				if (column == 2 && strcmp (";", value) != 0) {
+					strcat(str, ", asin");
+					strcat(str2, ", '");
+					strcat(str2, value);
+					strcat(str2, "'");
+				} 
 
-
-				if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-					res = PQexec(conn, "ROLLBACK");
-					do_exit(conn);
-				}
-				PQclear(res);
-				value = strtok(NULL, ", "); 
+				value = strtok(NULL, ",");
 				column++;
 			}
+			strcat(str, str2);
+			strcat(str, ");");
+			res = PQexec(conn, str);
+			if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+				do_exit(conn);
+			}
+			PQclear(res);
+			i++;
 		} 
-
-		res = PQexec(conn, "END");
+		res = PQexec(conn, "COMMIT");
 		printf("BEGIN END\n");
 		PQclear(res);
 		fclose(fp); 
 		end = clock();
 		time_spent = (double)(end-begin)/(CLOCKS_PER_SEC/1000);
-		printf("Tempo (em milissegundos): %lf", time_spent);
+		printf("\n\nTempo (em milissegundos): %lf", time_spent);
 		time_spent = (double)(end-begin)/CLOCKS_PER_SEC;
 		printf("\nTempo (em segundos): %lf \n", time_spent);
 }
@@ -74,30 +90,50 @@ void implicitTransition(PGconn *conn, PGresult *res, FILE* fp) {
 	int column = 0;
 	clock_t begin, end;
 	double time_spent;
-
+	int i = 0;
 	begin = clock();
 	printf("START\n");
 	while (fgets(buffer, buffer_size, fp)) {
-		char str[buffer_size] = "INSERT INTO product (product_name) VALUES ('";
+		char str[buffer_size] = "INSERT INTO product (";
+		char str2[buffer_size] = ") VALUES (";
 		column = 0; 
 		row++; 
 		if (row == 1) 
 			continue; 
+		char* value = strtok(buffer, ",");
+		while (value)  {
+			if (column == 0 && strcmp (";", value) != 0) {
+				strcat(str, "product_name");
+				strcat(str2, "'");
+				strcat(str2, value);
+				strcat(str2, "'");
+			} 
 
-		char* value = strtok(buffer, "\t"); 
+			if (column == 1 && strcmp (";", value) != 0) {
+				strcat(str, ", brand_name");
+				strcat(str2, ", '");
+				strcat(str2, value);
+				strcat(str2, "'");
+			} 
 
-		while (value) {
-			strcat(str, value);
-			strcat(str, "')");
-			res = PQexec(conn, str);
+			if (column == 2 && strcmp (";", value) != 0) {
+				strcat(str, ", asin");
+				strcat(str2, ", '");
+				strcat(str2, value);
+				strcat(str2, "'");
+			} 
 
-			if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-				do_exit(conn);
-			}
-			PQclear(res);
-			value = strtok(NULL, ", "); 
+			value = strtok(NULL, ",");
 			column++;
 		}
+		strcat(str, str2);
+		strcat(str, ");");
+		res = PQexec(conn, str);
+		if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+			do_exit(conn);
+		}
+		PQclear(res);
+		i++;
 	} 
 	printf("END");
 	fclose(fp); 
@@ -108,12 +144,18 @@ void implicitTransition(PGconn *conn, PGresult *res, FILE* fp) {
 	printf("\nTempo (em segundos): %lf \n", time_spent);
 }
 
+void explicitTransitionRollback(PGconn *conn, PGresult *res, FILE* fp) {
+	
+}
+
+void implicitTransitionRollback(PGconn *conn, PGresult *res, FILE* fp) {
+	
+}
+
 
 int main() {
 	PGconn *conn;
 	PGresult *res;
-
-
 	FILE* fp = fopen("data.csv", "r");
 
 	conn = PQconnectdb("user=postgres password=admin dbname=teste2");
@@ -131,10 +173,6 @@ int main() {
 		printf("\n\n\nSEM TRANSAÇÂO\n");
 		implicitTransition(conn, res, fp);
 	}
-
-
-	
-
 
 	return 0;
 }
