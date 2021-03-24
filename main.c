17cf1ae0fa1b35d26a18d851b4064c3ef448200b
natani3l <nataniel.lemes@estudante.uffs.edu.br>
2021-03-24 19:02:03 -0300

    insert only one column

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ void do_exit(PGconn *conn) {
 void explicitTransition(PGconn *conn, PGresult *res, FILE* fp) {
 		clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
 		char buffer[buffer_size];
-		int row = 0, column = 0, i = 0;
+		int row = 0, i = 0;
 		res = PQexec(conn, "BEGIN");
 		printf("BEGIN START\n");
 		if (PQresultStatus(res) != PGRES_COMMAND_OK) {
@@ -24,49 +24,31 @@ void explicitTransition(PGconn *conn, PGresult *res, FILE* fp) {
 			PQclear(res);
 			do_exit(conn);
 		}
+
 		while (fgets(buffer, buffer_size, fp)) {
-			char str[buffer_size] = "INSERT INTO product (";
-			char str2[buffer_size] = ") VALUES (";
+			char str[buffer_size] = "INSERT INTO product (product_name) VALUES ('";
 			row++; 
-			column = 0;
 			if (row == 1) 
 				continue; 
-			char* value = strtok(buffer, ",");
-			while (value)  {
-				if (column == 0 && strcmp ("null", value) != 0) {
-					strcat(str, "product_name");
-					strcat(str2, "'");
-					strcat(str2, value);
-					strcat(str2, "'");
-				} 
 
-				if (column == 1 && strcmp ("null", value) != 0) {
-					strcat(str, ", brand_name");
-					strcat(str2, ", '");
-					strcat(str2, value);
-					strcat(str2, "'");
-				} 
+			char* value = strtok(buffer, "\t"); 
 
-				if (column == 2 && strcmp ("null", value) != 0) {
-					strcat(str, ", asin");
-					strcat(str2, ", '");
-					strcat(str2, value);
-					strcat(str2, "'");
-				} 
+			while (value) {
+				strcat(str, value);
+				strcat(str, "')");
 
-				value = strtok(NULL, ",");
-				column++;
+				res = PQexec(conn, str);
+
+				if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+					res = PQexec(conn, "ROLLBACK");
+					do_exit(conn);
+				}
+				PQclear(res);
+				value = strtok(NULL, ", ");
 			}
-			strcat(str, str2);
-			strcat(str, ");");
-			res = PQexec(conn, str);
-			if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-				do_exit(conn);
-			}
-			PQclear(res);
-			i++;
-	} 
-		res = PQexec(conn, "COMMIT");
+		} 
+
+		res = PQexec(conn, "END");
 		printf("BEGIN END\n");
 		PQclear(res);
 		fclose(fp); 
@@ -76,51 +58,30 @@ void explicitTransition(PGconn *conn, PGresult *res, FILE* fp) {
 
 void implicitTransition(PGconn *conn, PGresult *res, FILE* fp) {
 	char buffer[buffer_size]; 
-	int row = 0, column = 0, i = 0;
+	int row = 0, i = 0;
 	clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
-	double time_spent;
 	printf("START\n");
 	while (fgets(buffer, buffer_size, fp)) {
-		char str[buffer_size] = "INSERT INTO product (";
-		char str2[buffer_size] = ") VALUES (";
-		column = 0; 
+		char str[buffer_size] = "INSERT INTO product (product_name) VALUES ('"; 
 		row++; 
 		if (row == 1) 
 			continue; 
-		char* value = strtok(buffer, ",");
-		while (value)  {
-			if (column == 0 && strcmp ("null", value) != 0) {
-				strcat(str, "product_name");
-				strcat(str2, "'");
-				strcat(str2, value);
-				strcat(str2, "'");
-			} 
 
-			if (column == 1 && strcmp ("null", value) != 0) {
-				strcat(str, ", brand_name");
-				strcat(str2, ", '");
-				strcat(str2, value);
-				strcat(str2, "'");
-			} 
+		char* value = strtok(buffer, "\t"); 
 
-			if (column == 2 && strcmp ("null", value) != 0) {
-				strcat(str, ", asin");
-				strcat(str2, ", '");
-				strcat(str2, value);
-				strcat(str2, "'");
-			} 
+		while (value) {
+			strcat(str, value);
+			strcat(str, "')");
 
-			value = strtok(NULL, ",");
-			column++;
+			res = PQexec(conn, str);
+
+			if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+				res = PQexec(conn, "ROLLBACK");
+				do_exit(conn);
+			}
+			PQclear(res);
+			value = strtok(NULL, ", ");
 		}
-		strcat(str, str2);
-		strcat(str, ");");
-		res = PQexec(conn, str);
-		if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-			do_exit(conn);
-		}
-		PQclear(res);
-		i++;
 	} 
 	printf("END");
 	fclose(fp); 
@@ -128,8 +89,46 @@ void implicitTransition(PGconn *conn, PGresult *res, FILE* fp) {
 	printf ("Total time = %f seconds\n", (end.tv_nsec - begin.tv_nsec) / 1000000000.0 + (end.tv_sec  - begin.tv_sec));
 }
 
-void rollback_erro(PGconn *conn, PGresult *res) {
-	
+void rollback_erro(PGconn *conn, PGresult *res, FILE* fp) {
+	char buffer[buffer_size]; 
+	int row = 0, i = 0;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
+		res = PQexec(conn, "BEGIN");
+	printf("BEGIN START FUNCTION ROLLBACK\n");
+	while (fgets(buffer, buffer_size, fp)) {
+		char str[buffer_size] = "INSERT INTO product (product_name) VALUES ('"; 
+		row++; 
+		if (row == 1) 
+			continue; 
+
+		char* value = strtok(buffer, "\t"); 
+
+		while (value) {
+			if (strcmp (value, "ERROR") == 0) {
+				printf("ERRO");
+				strcat(str, value);
+				strcat(str, "')]");
+			} else {
+				strcat(str, value);
+				strcat(str, "')");
+			}
+
+			res = PQexec(conn, str);
+
+			if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+				printf("SOMETHING WRONG WAS HAPPENED... ROLLBACK\n");
+				res = PQexec(conn, "ROLLBACK");
+				do_exit(conn);
+			}
+			PQclear(res);
+			value = strtok(NULL, ", ");
+		}
+	} 
+	printf("END COMMIT FUNCTION ROLLBACK\n");
+	res = PQexec(conn, "COMMIT");
+	fclose(fp); 
+	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+	printf ("Total time = %f seconds\n", (end.tv_nsec - begin.tv_nsec) / 1000000000.0 + (end.tv_sec  - begin.tv_sec));
 }
 
 void rollback(PGconn *conn, PGresult *res, FILE* fp) {
@@ -146,13 +145,31 @@ void rollback(PGconn *conn, PGresult *res, FILE* fp) {
 	while (fgets(buffer, buffer_size, fp)) {
 		char str[buffer_size] = "INSERT INTO product (";
 		char str2[buffer_size] = ") VALUES (";
+		
 		row++; 
 		column = 0;
 		if (row == 1) 
 			continue; 
 		char* value = strtok(buffer, ",");
+		if (strcmp ("ERRO", value) == 0) {
+			printf("FUNÇÃO DE ROLLBACK\n");
+			res = PQexec(conn, "ROLLBACK");
+			if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+				printf("ROLLBACK ERROR %s", PQerrorMessage(conn));
+				PQclear(res);
+				break;
+			}
+		}
 		while (value)  {
-			if (strcmp ("ERRO", value) == 0) {
+			
+			
+			if (column == 0 && strcmp ("null", value) != 0) {
+				strcat(str, "product_name");
+				strcat(str2, "'");
+				strcat(str2, value);
+				strcat(str2, "'");
+			}
+			if (column == 0 && strcmp ("ERRO", value) != 0) {
 				printf("FUNÇÃO DE ROLLBACK\n");
 				res = PQexec(conn, "ROLLBACK");
 				if (PQresultStatus(res) != PGRES_COMMAND_OK) {
@@ -160,13 +177,6 @@ void rollback(PGconn *conn, PGresult *res, FILE* fp) {
 					PQclear(res);
 					break;
 				}
-			}
-			
-			if (column == 0 && strcmp ("null", value) != 0) {
-				strcat(str, "product_name");
-				strcat(str2, "'");
-				strcat(str2, value);
-				strcat(str2, "'");
 			} 
 
 			if (column == 1 && strcmp ("null", value) != 0) {
@@ -174,6 +184,15 @@ void rollback(PGconn *conn, PGresult *res, FILE* fp) {
 				strcat(str2, ", '");
 				strcat(str2, value);
 				strcat(str2, "'");
+			}
+			if (column == 1 && strcmp ("ERRO", value) != 0) {
+				printf("FUNÇÃO DE ROLLBACK\n");
+				res = PQexec(conn, "ROLLBACK");
+				if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+					printf("ROLLBACK ERROR %s", PQerrorMessage(conn));
+					PQclear(res);
+					break;
+				}
 			} 
 
 			if (column == 2 && strcmp ("null", value) != 0) {
@@ -181,6 +200,15 @@ void rollback(PGconn *conn, PGresult *res, FILE* fp) {
 				strcat(str2, ", '");
 				strcat(str2, value);
 				strcat(str2, "'");
+			}
+			if (column == 2 && strcmp ("ERRO", value) != 0) {
+				printf("FUNÇÃO DE ROLLBACK\n");
+				res = PQexec(conn, "ROLLBACK");
+				if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+					printf("ROLLBACK ERROR %s", PQerrorMessage(conn));
+					PQclear(res);
+					break;
+				}
 			} 
 
 			value = strtok(NULL, ",");
@@ -202,6 +230,7 @@ void rollback(PGconn *conn, PGresult *res, FILE* fp) {
 		clock_gettime(CLOCK_MONOTONIC_RAW, &end);
 		printf ("Total time = %.2f seconds\n", (end.tv_nsec - begin.tv_nsec) / 1000000000.0 + (end.tv_sec  - begin.tv_sec));
 }
+
 
 int main() {
 	PGconn *conn;
@@ -223,7 +252,7 @@ int main() {
 		// printf("\nSEM TRANSAÇÂO\n");
 		// implicitTransition(conn, res, fp);
 		printf("ROLLBACK\n");
-		rollback(conn, res, fp);
+		rollback_erro(conn, res, fp);
 	}
 
 	return 0;
